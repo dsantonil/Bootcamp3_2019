a1ec78ca3cb782a5c4cc0e6569d2cb10cf3232b1
dsantonil <dsantonil@ufl.edu>
2019-10-02 00:54:45 -0400

    Updated listings.server.model.js

--- Bootcamp3/server/models/listings.server.model.js
+++ Bootcamp3/server/models/listings.server.model.js
@@ -7,13 +7,30 @@ var mongoose = require('mongoose'),
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here from Bootcamp Assignment #2 - ListingSchema.js File*/
-    
+  code: {type: String, required: true, unique: true},
+  name: {type: String, required: true},
+  coordinates:
+  {
+    latitude: {type: Number},
+    longitude: {type: Number},
+  },
+  address: {type: String},
+  created_at: {type: Date},
+  updated_at: {type: Date},
 });
 
 /* create a 'pre' function that adds the updated_at and created_at if not already there property */
 listingSchema.pre('save', function(next) {
   /* your code here from Bootcamp Assignment #2 - ListingSchema.js File */
 
+  var currentDate = new Date();
+
+  this.updated_at = currentDate;
+
+  if(!this.created_at){
+    this.created_at = currentDate;
+  }
+  
   next();
 });
 
